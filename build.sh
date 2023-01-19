dae5627703827d69ee8f8c2ee2e33234b1d0e2c0
Elia Pasquali <45105755+eliapasquali@users.noreply.github.com>
2023-01-19 17:34:35 +0100

    Test nuova action

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ shopt -s expand_aliases
 
 # Alias
 alias compile='latexmk -pdf -pdflatex="pdflatex -shell-escape"'
+alias clean='latexmk -c 1>/dev/null'
 
 echo "Compilazioni documenti"
 if [ -d "out" ]; then
@@ -16,7 +17,7 @@ cd analisi_capitolati/
 echo "Analisi capitolati"
 compile main.tex 1>/dev/null
 mv main.pdf ../out/analisi_capitolati.pdf
-latexmk -c 1>/dev/null
+clean
 cd ..
 
 # Dichirazione di impegni
@@ -24,7 +25,7 @@ cd dichiarazione_impegni/
 echo "Dichiarazione impegni"
 compile main.tex 1>/dev/null
 mv main.pdf ../out/dichiarazione_impegni.pdf
-latexmk -c 1>/dev/null
+clean
 cd ..
 
 # Lettera
@@ -32,7 +33,7 @@ cd lettera_candidatura/
 echo "Lettera di candidatura"
 compile main.tex 1>/dev/null
 mv main.pdf ../out/lettera_candidatura.pdf
-latexmk -c 1>/dev/null
+clean
 cd ..
 
 # Analisi requisiti
@@ -40,7 +41,7 @@ cd analisi_requisiti/
 echo "Analisi requisiti"
 compile main.tex 1>/dev/null
 mv main.pdf ../out/analisi_requisiti.pdf
-latexmk -c 1>/dev/null
+clean
 cd ..
 
 # Glossario
@@ -48,7 +49,7 @@ cd glossario/
 echo "Glossario"
 compile main.tex 2>/dev/null 1>/dev/null
 mv main.pdf ../out/glossario.pdf
-latexmk -c 1>/dev/null
+clean
 rm main.gl* main.ist
 cd ..
 
@@ -57,7 +58,7 @@ cd norme_progetto/
 echo "Norme progetto"
 compile main.tex 2>/dev/null 1>/dev/null
 mv main.pdf ../out/norme_progetto.pdf
-latexmk -c 1>/dev/null
+clean
 cd ..
 
 # Verbali
@@ -71,7 +72,7 @@ do
 	cd $D
 	compile main.tex 1>/dev/null
 	mv main.pdf ../out/$ver.pdf
-	latexmk -c 1>/dev/null
+	clean
 	cd ..
 done
 mv out/ ../out/verbali
