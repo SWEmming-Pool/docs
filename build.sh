dac2137ec1fc29373fb400d6a9f42f4a17eef4e5
Elia Pasquali <45105755+eliapasquali@users.noreply.github.com>
2023-01-19 17:13:41 +0100

    Corrette firstpage
    Centrate immagini e mail
    Aggiornato build script
    Aggiornato gitignore

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+shopt -s expand_aliases
+
+# Alias
+alias compile='latexmk -pdf -pdflatex="pdflatex -shell-escape"'
 
 echo "Compilazioni documenti"
 if [ -d "out" ]; then
@@ -10,7 +14,7 @@ mkdir out
 # Analisi capitolati
 cd analisi_capitolati/
 echo "Analisi capitolati"
-latexmk -pdf main.tex 1>/dev/null
+compile main.tex 1>/dev/null
 mv main.pdf ../out/analisi_capitolati.pdf
 latexmk -c 1>/dev/null
 cd ..
@@ -18,7 +22,7 @@ cd ..
 # Dichirazione di impegni
 cd dichiarazione_impegni/
 echo "Dichiarazione impegni"
-latexmk -pdf main.tex 1>/dev/null
+compile main.tex 1>/dev/null
 mv main.pdf ../out/dichiarazione_impegni.pdf
 latexmk -c 1>/dev/null
 cd ..
@@ -26,7 +30,7 @@ cd ..
 # Lettera
 cd lettera_candidatura/
 echo "Lettera di candidatura"
-latexmk -pdf main.tex 1>/dev/null
+compile main.tex 1>/dev/null
 mv main.pdf ../out/lettera_candidatura.pdf
 latexmk -c 1>/dev/null
 cd ..
@@ -34,7 +38,7 @@ cd ..
 # Analisi requisiti
 cd analisi_requisiti/
 echo "Analisi requisiti"
-latexmk -pdf main.tex 1>/dev/null
+compile main.tex 1>/dev/null
 mv main.pdf ../out/analisi_requisiti.pdf
 latexmk -c 1>/dev/null
 cd ..
@@ -42,7 +46,7 @@ cd ..
 # Glossario
 cd glossario/
 echo "Glossario"
-latexmk -pdf main.tex 2>/dev/null 1>/dev/null
+compile main.tex 2>/dev/null 1>/dev/null
 mv main.pdf ../out/glossario.pdf
 latexmk -c 1>/dev/null
 rm main.gl* main.ist
@@ -51,7 +55,7 @@ cd ..
 # Norme di progetto
 cd norme_progetto/
 echo "Norme progetto"
-latexmk -pdf main.tex 2>/dev/null 1>/dev/null
+compile main.tex 2>/dev/null 1>/dev/null
 mv main.pdf ../out/norme_progetto.pdf
 latexmk -c 1>/dev/null
 cd ..
@@ -65,7 +69,7 @@ do
 	ver=$(cut -c2- <<< $D)
 	echo $ver
 	cd $D
-	latexmk -pdf main.tex 1>/dev/null
+	compile main.tex 1>/dev/null
 	mv main.pdf ../out/$ver.pdf
 	latexmk -c 1>/dev/null
 	cd ..
