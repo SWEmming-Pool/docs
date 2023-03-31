7e5a48eb46448802bf7a6ab4a56c5a46354fbe2d
Elia Pasquali <45105755+eliapasquali@users.noreply.github.com>
2023-03-31 17:51:06 +0200

    Corretti errori comandi latex

--- people_randomizer.py
+++ people_randomizer.py
@@ -12,9 +12,9 @@ members = [
 
 shuffle = random.sample(members, 5)
 
-approvers = "\\renewcommand{\\documentApprovers\}\{%s \\smallskip}" % (shuffle[0])
-editors = "\\renewcommand{\\documentEditors\}{%s \\\\ & %s \\smallskip}" % (shuffle[1], shuffle[2])
-verifiers = "\\renewcommand{\\documentVerifiers\}{%s \\\\ & %s \\smallskip}" % (shuffle[3], shuffle[4])
+approvers = "\\renewcommand{\\documentApprovers}{%s \\smallskip}" % (shuffle[0])
+editors = "\\renewcommand{\\documentEditors}{%s \\\\ & %s \\smallskip}" % (shuffle[1], shuffle[2])
+verifiers = "\\renewcommand{\\documentVerifiers}{%s \\\\ & %s \\smallskip}" % (shuffle[3], shuffle[4])
 
 print(approvers)
 print(editors)
